1a0f8596a21bf4e7973f06fb084435c9a52d50cc
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-04-10 09:07:07 -0300

    base do projeto 2.0

--- Assets/_Sparky's Despair/Scripts/PlayerMovement.cs
+++ Assets/_Sparky's Despair/Scripts/PlayerMovement.cs
@@ -10,42 +10,62 @@ public class PlayerMovement : MonoBehaviour
     float dirZ;
     bool flipCharacter = true;
     public bool PodeMoverPersonagem = false;
+    public bool podeMoverParaEsquerda = false;
+    public bool podeMoverParaDireita = false;
+    public bool podeMoverParaCima = false;
+    public bool podeMoverParaBaixo = false;
+
+
     public GameObject personagemquevira;
+
 
     void Start()
     {
         PodeMoverPersonagem = true;
+        podeMoverParaEsquerda = true;
+        podeMoverParaDireita = true;
+        podeMoverParaCima = true;
+        podeMoverParaBaixo = true;
     }
 
     void gambiarra()
     {
         PodeMoverPersonagem = true;
-    } 
+    }
+
+
+
+
+
 
     void MovePersonagem()
     {
         PodeMoverPersonagem = false;
 
-        if (dirX > 0)
+        //Direita
+        if (dirX > 0 && podeMoverParaDireita == true)
         {
-            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x +.25f, gameObject.transform.position.y, gameObject.transform.position.z);
-            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x +.25f, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x + 1f, gameObject.transform.position.y, gameObject.transform.position.z);
+            //  gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x +.25f, gameObject.transform.position.y, gameObject.transform.position.z);
             if (!flipCharacter)
                 flip();
         }
-        else if (dirX < 0)
+        // Esquerda
+        if (dirX < 0 && podeMoverParaEsquerda == true)
         {
-            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x - .5f, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x - 1f, gameObject.transform.position.y, gameObject.transform.position.z);
             if (flipCharacter)
                 flip();
         }
-        else if (dirZ > 0)
+        //cima
+        if (dirZ > 0 && podeMoverParaCima == true)
         {
-            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + .7f);
+            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1f);
         }
-        else if (dirZ < 0)
+        //baixo
+        if (dirZ < 0 && podeMoverParaBaixo == true)
         {
-            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - .7f);
+            gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 1f);
         }
 
         Invoke("gambiarra", .3f);
@@ -56,16 +76,15 @@ public class PlayerMovement : MonoBehaviour
         flipCharacter = !flipCharacter;
         personagemquevira.GetComponent<Transform>().Rotate(0, 180, 0);
     }
-void Update()
+    void Update()
     {
 
         dirX = Input.GetAxis("Horizontal");
         dirZ = Input.GetAxis("Vertical");
 
 
-        if (dirX != 0||dirZ!=0)
+        if (dirX != 0 || dirZ != 0)
         {
-
 
             if (PodeMoverPersonagem == true)
             {
@@ -73,9 +92,24 @@ void Update()
                 MovePersonagem();
 
             }
+
         }
 
+        //void OnCollisionEnter(Collision collision)
+        //{
+        //    if (collision.gameObject.layer == LayerMask.NameToLayer("blockpath"))
+        //    {
+        //        PodeMoverPersonagem = false; // Interrompe o movimento do personagem ao colidir com um obstáculo
+        //    }
+        //}
+
+        //void OnCollisionExit(Collision collision)
+        //{
+        //    if (collision.gameObject.layer == LayerMask.NameToLayer("blockpath"))
+        //    {
+        //        PodeMoverPersonagem = true; // Permite que o personagem se mova novamente após sair da colisão com um obstáculo
+        //    }
+        //}
     }
 }
-
 
