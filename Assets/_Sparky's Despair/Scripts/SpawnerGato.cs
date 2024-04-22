ee7d667d1399964eddc3b156b8a9ba59f6a60077
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-04-22 16:38:36 -0300

    gatos aleatorios huehuehue

--- Assets/_Sparky's Despair/Scripts/SpawnerGato.cs
+++ Assets/_Sparky's Despair/Scripts/SpawnerGato.cs
@@ -4,38 +4,45 @@ using UnityEngine;
 
 public class SpawnerGato : MonoBehaviour
 {
-    public GameObject GatoI;
+    public GameObject[] gatoOpcao;
     public float TempoSpawn;
     public float TempoEntre;
+    public GameObject gatoPrefab;
 
     void Start()
     {
         TempoSpawn = 5f;
-        TempoEntre = .3f;
+        TempoEntre = .6f;
         StartCoroutine(SpawnCat());
     }
 
     IEnumerator SpawnCat()
     {
-        Instantiate(GatoI, transform.position, Quaternion.identity);
+        DefinirGato();
+        Instantiate(gatoPrefab, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(TempoEntre);
 
-        Instantiate(GatoI, transform.position, Quaternion.identity);
+        DefinirGato();
+        Instantiate(gatoPrefab, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(TempoEntre);
 
-        Instantiate(GatoI, transform.position, Quaternion.identity);
+        DefinirGato();
+        Instantiate(gatoPrefab, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(TempoEntre);
 
-        Instantiate(GatoI, transform.position, Quaternion.identity);
+        DefinirGato();
+        Instantiate(gatoPrefab, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(TempoSpawn);
 
+        DefinirGato();
         StartCoroutine(SpawnCat());
 
 
     }
 
-    void Update()
+    void DefinirGato()
     {
-        
+        int randomIndex = Random.Range(0, gatoOpcao.Length);
+        gatoPrefab = gatoOpcao[randomIndex];
     }
 }
