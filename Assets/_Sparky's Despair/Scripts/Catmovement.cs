df56c777ebeef43fb4f06dcacac2903648e08581
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-05-01 20:58:05 -0300

    beta do jogo

--- Assets/_Sparky's Despair/Scripts/Catmovement.cs
+++ Assets/_Sparky's Despair/Scripts/Catmovement.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Catmovement : MonoBehaviour
 {
+    public GameObject Spawner;
+
     public float CatVelocidade;
     public float CatVida;
     private float Tempo = 0f;
+    private float PosicaoI;
 
     void Start()
     {
         CatVelocidade = 5f;
         CatVida = 3f;
+        PosicaoI = gameObject.transform.position.x;
+        if (PosicaoI > 0f)
+        {
+            transform.Rotate(-60, 180, 0); 
+        }
     }
 
     void Update()
     {
+        
+        
         transform.Translate(Vector3.right * CatVelocidade * Time.deltaTime);
         Tempo += Time.deltaTime;
 
@@ -23,5 +34,7 @@ public class Catmovement : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        
+        
     }
 }
